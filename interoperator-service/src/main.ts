ebc964bfe851ec411657368e5263de8c4c09d7d4
Pamilo <81716232+Pamilo@users.noreply.github.com>
2025-04-30 15:07:19 -0500

    Update for the runing in docker

--- interoperator-service/src/main.ts
+++ interoperator-service/src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
 dotenv.config({ path: './src/Config/dev.env' });
-
+//docker run -p 8080:3000 --env-file ./src/config/dev.env interoperator-service
+console.log('API_BASE_URL:', process.env.API_BASE_URL);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('comunication'); // Set global prefix for routes
