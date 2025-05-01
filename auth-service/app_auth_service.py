98d9008d1f251485c5069e7eb6383632dfd30ff4
Sergio Córdoba <scordobamuriel@gmail.com>
2025-05-01 18:57:46 -0500

    add cors

--- auth-service/app_auth_service.py
+++ auth-service/app_auth_service.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from user.user import User
 
 app = Flask(__name__)
+CORS(app)
 
 user_service = User()
 
 @app.route("/register", methods=["POST"])
+@cross_origin()
 def register():
     data = request.get_json()
     response, status = user_service.register(data)
